6117afaa0d9491a8d6c8d28cc915cd3b5cad5835
Mathilde <ducros.mathilde@gmail.com>
2023-03-10 16:31:52 +0100

    depoiement

---
+++ url.js
@@ -0,0 +1,5 @@
+const BACKEND_URL = "https://backend-cometcall.vercel.app";
+const FRONTEND_URL =
+  "https://expo.dev/@violette923121/com-et-call?serviceType=classic&distribution=expo-go";
+
+module.exports = { BACKEND_URL, FRONTEND_URL };
